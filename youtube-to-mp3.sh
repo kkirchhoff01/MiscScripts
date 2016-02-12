667ffd6248689ab5dd492994e0b0c4cf63bb77d7
Kevin Kirchhoff <kkirchhoff01@yahoo.com>
2016-02-12 13:32:27 -0600

    Convert youtube video to mp3 with yotube-dl/avconv

---
+++ youtube-to-mp3.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Input parameters
+URL=$1      # YouTube video URL
+NAME=$2     # Desired name of audio file
+
+youtube-dl -o ~/Downloads/$NAME.mp4 -f mp4 "$URL"               # Download video as mp4
+avconv -i ~/Downloads/$NAME.mp4 -vn -f mp3 ~/Music/$NAME.mp3    # Convert to mp3
+
+# Remove mp4 from downloads
+if [ -e ~/Downloads/$NAME.mp4 ]
+then
+    echo "Removing ~/Downloads/$NAME.mp4"
+    rm ~/Downloads/$NAME.mp4
+fi
